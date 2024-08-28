877a04201cd8fad02a59c9e27eb8b5fdf971c0f5
peterhpo <peter.hjelle.petersenoeverleir@gmail.com>
2024-08-28 15:34:04 +0200

    Small updates and simplifications

--- fui_kk/sort_downloads.py
+++ fui_kk/sort_downloads.py
@@ -1,93 +1,78 @@
 #!/usr/bin/env python3
-"""Script to sort downloaded data (html and tsv) into folder structure
-automatically based on file names(form titles)."""
+"""Automatically sort downloaded data (html and csv) into folder structure based on file names."""
 
-__authors__    = ["Ole Herman Schumacher Elgesem"]
-__copyright__  = "Ole Herman Schumacher Elgesem"
-__credits__    = ["Erik Vesteraas"]
-__license__    = "MIT"
-# This file is subject to the terms and conditions defined in
-# file 'LICENSE.txt', which is part of this source code package.
+__authors__ = ["Ole Herman Schumacher Elgesem", "Peter Hjelle Petersen-Øverleir"]
+__modified_by__ = ["Peter Hjelle Petersen-Øverleir"]
+__copyright__ = "Ole Herman Schumacher Elgesem"
+__credits__ = ["Erik Vesteraas"]
+__license__ = "MIT"
 
 import os
-import sys
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import re
+import sys
 from shutil import copyfile
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from file_funcs import path_join
 
 def get_args():
-    ap = ArgumentParser(description='Sort downloads into folder structure',
-                        formatter_class=ArgumentDefaultsHelpFormatter)
-    ap.add_argument('--input', '-i', type=str, default='./downloads',
-                    help='Input directory')
-    ap.add_argument('--output', '-o', type=str, default='./data',
-                    help='Output directory')
-    ap.add_argument('--verbose', '-v', action="store_true",
-                    help='Print moves')
-    ap.add_argument('--delete', '-d', action="store_true",
-                    help='Delete moved files')
-    ap.add_argument('--exclude', '-e', type=str,
-                    help=r'Exclude regex',
-                    default=r'(testskjema)|(XXX)|(\*\*\*)')
-    args = ap.parse_args()
-
-    return args
+    parser = ArgumentParser(description='Sort downloads into folder structure',
+                            formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-i', '--input', type=str, default='./downloads', help='Input directory')
+    parser.add_argument('-o', '--output', type=str, default='./data', help='Output directory')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print moves')
+    parser.add_argument('-d', '--delete', action='store_true', help='Delete moved files')
+    parser.add_argument('-e', '--exclude', type=str, default=r'(testskjema)|(XXX)|(\*\*\*)', help='Exclude regex')
+    return parser.parse_args()
 
 def main():
     args = get_args()
-    delete = args.delete
     exclude_pattern = re.compile(args.exclude)
     semester_pattern = re.compile(r'(V|H)[0-9]{4}')
-    course_code_pattern = re.compile(r'(([A-Z]{1,5}-){0,1}[A-Z]{1,5}[0-9]{3,4})([A-Z]{1,5}){0,1}')
-    for root, subdirs, files in os.walk(args.input):
+    course_code_pattern = re.compile(r'(([A-Z]{1,5}-)?[A-Z]{1,5}[0-9]{3,4})([A-Z]{1,5})?')
+
+    for root, _, files in os.walk(args.input):
         for file_x in files:
             path = path_join(root, file_x)
-            filename, extension = os.path.splitext(path)
-            m = exclude_pattern.search(path)
-            if m is not None or path[0] == ".":
-                print("Excluded: " + path)
+            if exclude_pattern.search(path):
                 continue
-            m = semester_pattern.search(path)
-            if m is None:
-                print("Skipped - No semester: " + path)
-                continue
-            semester = m.group(0)
-            m = course_code_pattern.search(path)
-            if m is None:
-                print("Skipped - No course code: " + path)
-                continue
-            course = m.group(0)
 
-            dir_name = extension[1:]
-            if dir_name == "json":
-                dir_name = "participation"
-            target_folder = path_join(args.output, semester, "downloads", dir_name)
-            os.makedirs( target_folder, exist_ok=True )
-            newpath = path_join(target_folder, course + extension )
+            semester_match = semester_pattern.search(path)
+            course_match = course_code_pattern.search(path)
 
-            if delete:
-                # I hate windows:
+            if not semester_match or not course_match:
+                if args.verbose:
+                    print(f"Skipped - {'No semester' if not semester_match else 'No course code'}: {path}")
+                continue
+
+            extension = os.path.splitext(path)[1][1:]
+            folder_type = 'participation' if extension == 'json' else extension
+
+            target_folder = path_join(args.output, semester_match.group(0), 'downloads', folder_type)
+            os.makedirs(target_folder, exist_ok=True)
+
+            new_path = path_join(target_folder, course_match.group(0) + '.' + extension)
+            
+            if args.delete:
                 try:
-                    os.remove(newpath)
+                    os.remove(new_path)
                 except:
                     pass
-                os.rename(path, newpath)
+                os.rename(path, new_path)
             else:
-                copyfile(path, newpath)
-            if args.verbose:
-                print(path)
-                print(" -> "+newpath)
-                print(root)
+                copyfile(path, new_path)
 
-    while delete:
-        delete = False
-        for root, subdirs, files in os.walk(args.input):
-            if len(subdirs) == 0 and len(files) == 0:
-                os.rmdir(root)
-                if args.verbose:
-                    print("rm: "+path)
-                delete = True
+            if args.verbose:
+                print(f"{path} -> {new_path}")
+
+    if args.delete:
+        while args.delete:
+            args.delete = False
+            for root, subdirs, files in os.walk(args.input):
+                if not subdirs and not files:
+                    os.rmdir(root)
+                    if args.verbose:
+                        print(f"Removed empty directory: {root}")
+                    args.delete = True
 
 if __name__ == '__main__':
     main()
