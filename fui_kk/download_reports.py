306f46e0905587de12aea312cfb44dc928d08aa6
peterhpo <peter.hjelle.petersenoeverleir@gmail.com>
2024-08-28 15:54:30 +0200

    Changed the download fucntionality to use the Nettskjema API as the old way was deprecated

--- fui_kk/download_reports.py
+++ fui_kk/download_reports.py
@@ -1,116 +1,45 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Downloads TSV and HTML reports for a user from nettskjema.uio.no"""
+"""Downloads CSV reports for a user from nettskjema.uio.no using OAuth 2.0"""
 
-__authors__    = ["Erik Vesteraas"]
-__copyright__  = "Erik Vesteraas"
-__license__    = "MIT"
+__authors__ = ["Erik Vesteraas", "Peter Hjelle Petersen-Øverleir"]
+__modified_by__ = ["Peter Hjelle Petersen-Øverleir"]
+__copyright__ = "Erik Vesteraas"
+__license__ = "MIT"
 # This file is subject to the terms and conditions defined in
 # file 'LICENSE.txt', which is part of this source code package.
 
-# Dependencies:
-# pip install selenium
-# pip install requests
-# brew install phantomjs
-
-# Login and forms system requires javascript, so we use selenium to login and
-# browse the page.
-# Downloading files with selenium is not very convenient, so we use requests
-# for that particular part.
-# PhantomJS is the engine we use for selenium.
-
-import getpass
 import os
 import sys
 import locale
 import argparse
-import json
-import re
 import pickle
 import requests
-from selenium import webdriver
+import json
+from dotenv import load_dotenv
 
-from file_funcs import path_join, path_clean, filename_clean
+from file_funcs import path_join, path_clean, filename_clean, dump_json
+from api_funcs import obtain_token, save_token, api_request, load_token
 
 def get_args():
     argparser = argparse.ArgumentParser(description='Download report data from nettskjema.uio.no')
     argparser.add_argument('--out', '-o', help='Output directory (default="./downloads")', type=str, default='./downloads')
     argparser.add_argument('--filter', '-f', help='String to filter by', type=str)
-    argparser.add_argument('--username', '-u', help='Username for login', type=str)
-    argparser.add_argument('--password', '-p', help='Password for login', type=str)
-    argparser.add_argument('--tsv', help='Download TSV files', action="store_true")
-    argparser.add_argument('--html', help='Download HTML reports', action="store_true")
-    argparser.add_argument('--stats', help='Download answer statistics', action="store_true")
+    argparser.add_argument('--csv', help='Download CSV files', action="store_true")
     args = argparser.parse_args()
 
-    if not (args.tsv or args.html or args.stats):
-        args.tsv = True
-        args.html = True
-        args.stats = True
-
-    if not args.username:
-        args.username = input('Username: ')
-
-    if not args.password:
-        args.password = getpass.getpass()
+    # Set all to True if none are provided
+    if not args.csv:
+        args.csv = True
 
     return args
-
-def login(driver, args):
-    driver.get('https://nettskjema.uio.no/user/index.html')
-
-    userfield = driver.find_element_by_css_selector('#username')
-    userfield.send_keys(args.username)
-
-    passfield = driver.find_element_by_css_selector('#password')
-    passfield.send_keys(args.password)
-
-    button = driver.find_element_by_css_selector('#login-box-form .submit')
-    button.click()
 
 def write_to_file(folder, name, extension, content):
     if not os.path.exists(folder):
         os.makedirs(folder)
-    filename = path_join(folder, name) + '.' + extension
-    filename = path_clean(filename)
+    filename = path_clean(path_join(folder, name) + '.' + extension)
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(content)
-
-def try_to_find_int(driver, selector):
-    try:
-        return int(driver.find_element_by_css_selector(selector).text)
-    except:
-        return 0
-
-def render_html(name, stats, content):
-    return '''
-    <html>
-        <head>
-            <meta charset="utf-8" />
-        </head>
-        <body>
-            <p>Delivered replies: {answered}</p>
-            <p>Commenced replies: {started}</p>
-            <p>Number of sent invitations: {invited}</p>
-            <hr />
-            <h1>{title}</h1>
-            {content}
-        </body>
-    </html>
-    '''.format(
-        title=name,
-        content=content,
-        answered=stats['answered'],
-        started=stats['started'],
-        invited=stats['invited']
-    )
-
-def get_id(url):
-    p = re.compile("id=[0-9]{1,10}")
-    m = p.search(url)
-    if m is None:
-        return ""
-    return m.group(0)[3:]
 
 def read_list(path):
     try:
@@ -121,7 +50,7 @@ def read_list(path):
 
 def read_binary(path):
     try:
-        with open (path, 'rb') as fp:
+        with open(path, 'rb') as fp:
             return pickle.load(fp)
     except FileNotFoundError:
         return None
@@ -139,100 +68,113 @@ def os_encode(msg):
 
 def error(msg, exception=None, *, label=None):
     if label:
-        print("\n***FUI-KK ERROR: {}***".format(label))
+        print(f"\n***FUI-KK ERROR: {label}***")
     else:
         print("\n***FUI-KK ERROR***")
     if exception:
-        print("Exception: {}\n".format(type(exception)))
+        print(f"Exception: {type(exception)}\nMessage: {exception}\n")
     print(msg)
-    sys.exit(-1)
+    # sys.exit(-1)
 
-def download_files(driver, args):
-    downloaded = read_list(args.out+"/downloaded.txt")
+def download_files(args):
+    downloaded = read_list(args.out + "/downloaded.txt")
 
-    formdata = read_binary(args.out+"/formdata.dat")
+    formdata = read_binary(args.out + "/formdata.dat")
     if not formdata:
-        driver.get('https://nettskjema.uio.no/user/form/list.html')
-        forms = driver.find_elements_by_css_selector('.forms .formName')
-        formdata = [(form.text, form.get_attribute('href')) for form in forms]
-        write_binary(args.out+"/formdata.dat",formdata)
+        forms_url = "https://api.nettskjema.no/v3/form/me"
+        response = api_request(forms_url)
+        forms = response.json()
+        
+        # Check if forms contain expected keys
+        try:
+            formdata = [(form['title'], form['formId'], form['numberOfDeliveredSubmissions']) for form in forms]
+        except KeyError as e:
+            error(f"Key error: {e}. Expected keys not found in the response", e)
+        
+        write_binary(args.out + "/formdata.dat", formdata)
 
     if args.filter:
         filtered = [x for x in formdata if args.filter in x[0]]
-        print('Filter matched {} of {} forms'.format(len(filtered), len(formdata)))
+        print(f'Filter matched {len(filtered)} of {len(formdata)} forms')
         formdata = filtered
 
-    session = requests.Session()
-    cookies = driver.get_cookies()
-    for cookie in cookies:
-        session.cookies.set(cookie['name'], cookie['value'])
     out_path = path_clean(args.out)
-    tsv_path = path_join(out_path, 'tsv')
-    html_path = path_join(out_path, 'html')
+    csv_path = path_join(out_path, 'csv')
     stats_path = path_join(out_path, 'stats')
+    
+    stats_aggregated = {}
 
-    for (name, url) in formdata:
-        form_id = get_id(url)
-
+    for (name, form_id, number_of_delivered_submissions) in formdata:
         try:
-            if form_id in downloaded:
-                print("Skipping {} (id={})".format(name,form_id))
+            if str(form_id) in downloaded:
+                print(f"Skipping {name} (id={form_id})")
                 continue
-            print("Fetching {} (id={})".format(name,form_id))
+            print(f"Fetching {name} (id={form_id})")
         except UnicodeEncodeError as e:
-            # NOTE: This error can be fixed by using os_encode on name,
-            #       however I think it is useful to force windows users
-            #       to change to utf-8, just in case wrong encoding
-            #       causes problems elsewhere.
-            error_msg = "\n".join([
-            "Form id={}".format(form_id),
-            "Form name: {}".format(os_encode(name)),
-            "Your terminal probably doesn't like unicode.",
-            "To fix this on windows, change codepage using this command:",
-            "chcp 65001"
-            ])
-            error(error_msg, e, label="Non-unicode codepage")
-        results_url = url.replace('preview', 'results')
-        driver.get(results_url)
-        stats = {
-            'answered': try_to_find_int(driver, '.delivered-submissions .number'),
-            'started': try_to_find_int(driver, '.saved-submissions .number'),
-            'invited': try_to_find_int(driver, '.valid-invitations .number')
-        }
+            error(f"Form id={form_id}\nForm name: {os_encode(name)}", e, label="Non-unicode codepage")
+        
         name_cleaned = filename_clean(name)
-        if args.tsv:
-            tsv_url = url.replace('preview', 'download') + '&encoding=utf-8'
-            response = session.get(tsv_url)
-            write_to_file(tsv_path, name_cleaned, 'tsv', response.text)
 
-        if args.html:
-            html_url = url.replace('preview', 'report/web') + '&include-open=1&remove-profile=1'
-            response = session.get(html_url)
-            write_to_file(html_path, name_cleaned, 'html', render_html(name, stats, response.text))
+        # Fetch invitations data
+        try:
+            invites_url = f"https://api.nettskjema.no/v3/form/{form_id}/invitations"
+            response = api_request(invites_url)
+            if response.ok:
+                invitations = [json.loads(line) for line in response.text.splitlines()]
+                num_invited = len(invitations)
+                num_answered = number_of_delivered_submissions
+                response_rate = (num_answered / num_invited) * 100 if num_invited > 0 else 0
 
-        if args.stats:
-            stats_json = json.dumps(stats)
-            write_to_file(stats_path, name_cleaned, 'json', stats_json)
+                stats = {
+                    "started": 0,
+                    "answered": number_of_delivered_submissions,
+                    "invited": num_invited,
+                    "response_rate": response_rate
+                }
+                
+        except Exception as e:
+            error(f"Failed to fetch invitations for {name} (id={form_id})", label=f"HTTP {e}")
+            stats = {
+                "started": 0,
+                "answered": number_of_delivered_submissions,
+                "invited": 0,
+                "response_rate": 0
+            }
+                    
+        dump_json(stats, f"{stats_path}/{name_cleaned}.json")
 
-        with open(args.out+"/downloaded.txt", 'a') as f:
-            f.write(form_id+"\n")
+        # Aggregate statistics
+        stats_aggregated[name] = stats
+
+        if args.csv:
+            csv_url = f"https://api.nettskjema.no/v3/form/{form_id}/csv-report"
+            response = api_request(csv_url)
+            if response.ok:
+                write_to_file(csv_path, name_cleaned, 'csv', response.content.decode())
+            else:
+                error(f"Failed to fetch CSV report for {name} (id={form_id})", label=f"HTTP {response.status_code}")
+        
+        with open(args.out + "/downloaded.txt", 'a') as f:
+            f.write(str(form_id) + "\n")
+
+    # Write the aggregated stats to a file
+    stats_filename = 'stats.json'
+    dump_json({"respondents": stats_aggregated}, path_join(stats_path, stats_filename))
 
 def main():
+    load_dotenv()
+    
     args = get_args()
+    token_data = load_token()
 
-    driver = webdriver.PhantomJS()
-    driver.set_window_size(800, 600)
+    if not token_data:
+        token_data = obtain_token()
+        save_token(token_data)
 
     try:
-        login(driver, args)
-        download_files(driver, args)
+        download_files(args)
     except requests.exceptions.TooManyRedirects as e:
-        error("Sometimes nettskjema doesn't like us.\n"\
-              "Just wait a little while and continue\n"\
-              "by rerunning script.", e, label="Nettskjema")
-    finally:
-        driver.close()
-        driver.quit()
+        error("Sometimes nettskjema doesn't like us.\nJust wait a little while and continue\nby rerunning the script.", e, label="Nettskjema")
 
 if __name__ == '__main__':
     main()
